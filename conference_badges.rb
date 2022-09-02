7a860891acf10f5349373ad97bedc97ad54e6171
Esther2015157 <esthersly8@gmail.com>
2022-09-02 12:52:58 +0300

    Update heredoc info

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+describe 'conference_badges' do
+  let(:name) { "Arel" }
+  let(:attendees) { ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"] }
+  let(:badges) do 
+    [
+      "Hello, my name is Edsger.",
+      "Hello, my name is Ada.",
+      "Hello, my name is Charles.",
+      "Hello, my name is Alan.",
+      "Hello, my name is Grace.",
+      "Hello, my name is Linus.",
+      "Hello, my name is Matz."
+    ]
+  end
+  let(:room_assignments) do
+    [
+      "Hello, Edsger! You'll be assigned to room 1!",
+      "Hello, Ada! You'll be assigned to room 2!",
+      "Hello, Charles! You'll be assigned to room 3!",
+      "Hello, Alan! You'll be assigned to room 4!",
+      "Hello, Grace! You'll be assigned to room 5!",
+      "Hello, Linus! You'll be assigned to room 6!",
+      "Hello, Matz! You'll be assigned to room 7!"
+   ]
+  end
